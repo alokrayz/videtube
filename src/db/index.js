ab83fa643fff24e86beca7fc36f8bd67ba61889c
Alok Ray <alokawesome888@gmail.com>
2025-12-17 10:47:59 +0530

    MOngodb connected

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME} from "../constants.js";
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected to ${connectionInstance.connection.host} successfully`); // to show host name where db is connected
+    } catch (error) {
+        console.log("Error in DB connection", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,61 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./env"
+});
+
+// require("dotenv").config(); ----> ye wla import code kinconsistency ko khrab krta h so we will use diif f method
+
+
+
+// one way to connect mongodb is to write connection cod in maon index.js file by creating funvtion and using after it
+/*
+function connectDB() {}
+
+connectDB();
+alternative using iffe that is function after function ;()() semicolon is used to avoid error if some code is written after this function
+*/
+
+// other way is to create separate file for connection like db.js and export the function and import it in index.js and use it there
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async () => {
+    try {
+        mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+        app.on("err", (error) => {
+            console.log("ERR:", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("Error in DB connection", error);
+        throw error;
+    }
+})()
+
+*/
